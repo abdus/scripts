0a6589bff89cfef4f028f8535ca11221f35ff907
Abdus <abdus@abdus.net>
2024-11-05 21:01:28 +0530

    minor

--- git-extract-author-emails.sh
+++ git-extract-author-emails.sh
@@ -4,8 +4,8 @@ if [[ ! -d ".git" ]]; then
   echo "Not a Git Directory"
 fi
 
-git --no-pager log "$1" |
-  grep --regexp='^Author:\ *' |
+git --no-pager log |
+  grep --regexp='^Author: *' |
   grep -o --regexp='<.*>' |
   sed 's/[<|>]//g' |
   sort -u |
